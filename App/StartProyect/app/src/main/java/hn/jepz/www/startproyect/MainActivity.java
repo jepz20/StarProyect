0c2323230c2d10914448f80f3e43230e26e28ffd
jepz20 <phoenix01>
2015-12-12 14:29:13 -0600

    Se agrego activity de Login

--- App/StartProyect/app/src/main/java/hn/jepz/www/startproyect/MainActivity.java
+++ App/StartProyect/app/src/main/java/hn/jepz/www/startproyect/MainActivity.java
@@ -1,5 +1,6 @@
 package hn.jepz.www.startproyect;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,7 +11,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,9 +20,11 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        intent = new Intent(this, LoginActivity.class);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                startActivity(intent);
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
