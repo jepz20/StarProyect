8c4d224c67d0fdeb4e307f0f59870d83decbddba
jepz20 <phoenix01>
2015-12-12 13:54:52 -0600

    Inicio del proyecto

---
+++ App/StartProyect/app/src/test/java/hn/jepz/www/startproyect/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package hn.jepz.www.startproyect;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
