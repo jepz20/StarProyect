e7df201141e1ed9916d987da6f4c9c9682447774
jepz20 <phoenix01>
2015-12-12 21:59:22 -0600

    Volvi a empezar porque perdi todo con un reset

--- App/StartProyect/app/src/main/java/hn/jepz/www/startproyect/MainActivity.java
+++ App/StartProyect/app/src/main/java/hn/jepz/www/startproyect/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,8 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         intent = new Intent(this, LoginActivity.class);
