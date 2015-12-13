6a7fee7c3d97d52924789f89b64b80006de5c50c
jepz20 <phoenix01>
2015-12-13 00:40:58 -0600

    activity estrellas

---
+++ App/StartProyect/app/src/main/java/hn/jepz/www/startproyect/EstrellasActivity.java
@@ -0,0 +1,84 @@
+package hn.jepz.www.startproyect;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class EstrellasActivity extends Activity {
+    public static final String PREFERENCIAS_STAR = "PrefStarProyect";
+    public static final String PREF_ULTIMO_REPORTE = "PrefUltimoReporte";
+    public static final String PREF_AULA = "PrefAula";
+    public static final String PREF_DEPORTES = "PrefDeportes";
+    public static final String PREF_CULTURAL = "PrefCultural";
+    public static final String PREF_CIENCIA = "PrefCiencia";
+    public static final String PREF_INTERCAMBIO = "PrefIntercambio";
+    public static final String PRIMERA_VEZ_REGISTRO = "PrefPRimeraVez";
+    private SharedPreferences misPreferencias;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_estrellas);
+        misPreferencias = getSharedPreferences(PREFERENCIAS_STAR, Context.MODE_PRIVATE);
+        int estrellasSi = 0;
+        String mensaje ="";
+        if (misPreferencias.getInt(PREF_AULA,0) >=200) {
+            estrellasSi++;
+        }
+        if (misPreferencias.getBoolean(PREF_DEPORTES, false)) {
+            estrellasSi++;
+        }
+        if (misPreferencias.getBoolean(PREF_CULTURAL, false)) {
+            estrellasSi++;
+        }
+        if (misPreferencias.getBoolean(PREF_CIENCIA, false)) {
+            estrellasSi++;
+        }
+        if (misPreferencias.getBoolean(PREF_INTERCAMBIO, false)) {
+            estrellasSi++;
+        }
+
+        //Primera Estrella
+        ImageView ivEstella1 = (ImageView) findViewById(R.id.ivEstrella1);
+        ivEstella1.setBackgroundResource(R.drawable.estrella_contorno);
+        ImageView ivEstella2 = (ImageView) findViewById(R.id.ivEstrella2);
+        ivEstella2.setBackgroundResource(R.drawable.estrella_contorno);
+        ImageView ivEstella3 = (ImageView) findViewById(R.id.ivEstrella3);
+        ivEstella3.setBackgroundResource(R.drawable.estrella_contorno);
+        ImageView ivEstella4 = (ImageView) findViewById(R.id.ivEstrella4);
+        ivEstella4.setBackgroundResource(R.drawable.estrella_contorno);
+        ImageView ivEstella5 = (ImageView) findViewById(R.id.ivEstrella5);
+        ivEstella5.setBackgroundResource(R.drawable.estrella_contorno);
+        if (estrellasSi >=1) {
+            ivEstella1.setBackgroundResource(R.drawable.estrella_dorada);
+            mensaje = "Buen Inicio, siga reportando las actividades";
+        }
+        if (estrellasSi >=2) {
+            ivEstella2.setBackgroundResource(R.drawable.estrella_dorada);
+            mensaje = "Ya esta en el segundo paso para ser una escuela 5 Estellas";
+        }
+        if (estrellasSi >=3) {
+            ivEstella3.setBackgroundResource(R.drawable.estrella_dorada);
+            mensaje = "Ha llegado a medio camino, Animos";
+        }
+        if (estrellasSi >=4) {
+            ivEstella4.setBackgroundResource(R.drawable.estrella_dorada);
+            mensaje = "Ya casi llega a la meta, Adelante";
+        }
+        if (estrellasSi >=5) {
+            ivEstella5.setBackgroundResource(R.drawable.estrella_dorada);
+            mensaje = "Ya es una escuela 5 estrellas";
+        }
+        TextView tvTitulo = (TextView) findViewById(R.id.tituloEstrella);
+        tvTitulo.setText(mensaje);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        startActivity(new Intent(this,MainActivity.class));
+    }
+}
--- App/StartProyect/app/src/main/java/hn/jepz/www/startproyect/SiActivity.java
+++ App/StartProyect/app/src/main/java/hn/jepz/www/startproyect/SiActivity.java
@@ -1,19 +1,32 @@
 package hn.jepz.www.startproyect;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
 
 public class SiActivity extends AppCompatActivity {
-
+    public static final String PREFERENCIAS_STAR = "PrefStarProyect";
+    public static final String PREF_ULTIMO_REPORTE = "PrefUltimoReporte";
+    public static final String PREF_AULA = "PrefAula";
+    public static final String PREF_DEPORTES = "PrefDeportes";
+    public static final String PREF_CULTURAL = "PrefCultural";
+    public static final String PREF_CIENCIA = "PrefCiencia";
+    public static final String PREF_INTERCAMBIO = "PrefIntercambio";
+    private SharedPreferences misPreferencias;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         setContentView(R.layout.activity_si);
+        misPreferencias = getSharedPreferences(PREFERENCIAS_STAR, Context.MODE_PRIVATE);
         LinearLayout llAula = (LinearLayout) findViewById(R.id.llAula);
         LinearLayout llDeporte = (LinearLayout) findViewById(R.id.llDeporte);
         LinearLayout llCultural = (LinearLayout) findViewById(R.id.llCultural);
@@ -53,6 +66,44 @@ public class SiActivity extends AppCompatActivity {
             public void onClick(View v) {
                 CheckBox cb = (CheckBox) findViewById(R.id.cbIntercambio);
                 cb.setChecked(!cb.isChecked());
+            }
+        });
+
+        Button btnFinalizar = (Button) findViewById(R.id.btnFinalizar);
+        btnFinalizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean seleccionoAlguno = false;
+                SharedPreferences.Editor editor = misPreferencias.edit();
+                if (((CheckBox) findViewById(R.id.cbAula)).isChecked()) {
+                    int dias = misPreferencias.getInt(PREF_AULA,0);
+                    editor.putInt(PREF_AULA, dias + 1);
+                    seleccionoAlguno = true;
+                }
+                if (((CheckBox) findViewById(R.id.cbDeporte)).isChecked()) {
+                    editor.putBoolean(PREF_DEPORTES,true);
+                    seleccionoAlguno = true;
+                }
+                if (((CheckBox) findViewById(R.id.cbCultural)).isChecked()) {
+                    editor.putBoolean(PREF_CULTURAL,true);
+                    seleccionoAlguno = true;
+                }
+                if (((CheckBox) findViewById(R.id.cbCiencia)).isChecked()) {
+                    editor.putBoolean(PREF_CIENCIA,true);
+                    seleccionoAlguno = true;
+                }
+                if (((CheckBox) findViewById(R.id.cbIntercambio)).isChecked()) {
+                    editor.putBoolean(PREF_INTERCAMBIO,true);
+                    seleccionoAlguno = true;
+                }
+                editor.commit();
+                if (seleccionoAlguno) {
+                    startActivity(new Intent(getBaseContext(), EstrellasActivity.class));
+                } else {
+                    Snackbar.make(v, "Porfavor seleccione al menos una actividad", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+
+                }
             }
         });
     }
