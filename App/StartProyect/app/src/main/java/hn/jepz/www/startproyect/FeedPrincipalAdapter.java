c38ca35070e2c61097a4637964b39b8bc07f4ada
jepz20 <phoenix01>
2015-12-12 21:01:38 -0600

    Actividad Si

---
+++ App/StartProyect/app/src/main/java/hn/jepz/www/startproyect/FeedPrincipalAdapter.java
@@ -0,0 +1,7 @@
+package hn.jepz.www.startproyect;
+
+/**
+ * Created by jepz2_000 on 12/12/2015.
+ */
+public class FeedPrincipalAdapter {
+}
---
+++ App/StartProyect/app/src/main/java/hn/jepz/www/startproyect/SiActivity.java
@@ -0,0 +1,14 @@
+package hn.jepz.www.startproyect;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class SiActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_si);
+    }
+
+}
