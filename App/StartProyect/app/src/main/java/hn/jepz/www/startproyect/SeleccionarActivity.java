515eb84a9c49cfb55ed7fe00f54966cb61ebaa24
Eversonzs <eversonzs@hotmail.com>
2015-12-12 23:27:27 -0600

    Seleccionar centro activity

---
+++ App/StartProyect/app/src/main/java/hn/jepz/www/startproyect/SeleccionarActivity.java
@@ -0,0 +1,19 @@
+package hn.jepz.www.startproyect;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Depto.Sistemas on 12/12/2015.
+ */
+public class SeleccionarActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seleccionar);
+
+        
+
+    }
+}
